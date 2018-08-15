a918cd020a3e444447b4b9c9e06d45e09bc9cdd6
Duane Felton <dfelton2010@gmail.com>
2018-08-15 15:12:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,11 +6,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+<<<<<<< HEAD
 def valid_move?(board, index)
   if index.between?(0,8) && board[index] == " " 
     return true
   else 
     return false
+=======
+def valid_move?(board, position_taken)
+  if position_taken.between?(0,8) != true || board[position_taken] == "X"
+    return false
+  else 
+    return true
+>>>>>>> 1570c2af6bbdfe3236ea75e827d0aa6cd46c47e6
 end
 end
 
@@ -34,10 +42,20 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
+<<<<<<< HEAD
   if valid_move?(board, index)
     move(board, index, token = "X")
     display_board(board)
   else
     turn(board)
+=======
+  if valid_move?(board, index) == true && position_taken(board, index) == false
+    move(board, user_input, token = "X")
+    puts display_board(board)
+  else
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+>>>>>>> 1570c2af6bbdfe3236ea75e827d0aa6cd46c47e6
 end
 end
